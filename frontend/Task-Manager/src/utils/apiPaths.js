2142e3759cdcd31afe66ad4037707f0955dba3c7
chetan <ck769825@gmail.com>
2026-05-08 12:27:30 +0530

    Updated API URL to Railway backend

--- frontend/Task-Manager/src/utils/apiPaths.js
+++ frontend/Task-Manager/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://task-manager-backend-sya5.onrender.com";export const API_PATHS = {
+export const BASE_URL = "http://localhost:8000";
+export const API_PATHS = {
     AUTH: {
         SIGNUP: "/api/auth/register",
         LOGIN: "/api/auth/login",
